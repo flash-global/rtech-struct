03e43488e501157fe8ca6bba908438c73068f662
mafricalucas <lucasmafrica57@gmail.com>
2022-11-15 11:35:58 +0100

    remove union

--- structures/legacy_agreed_price.js
+++ structures/legacy_agreed_price.js
@@ -1,7 +1,7 @@
 const s = require('superstruct')
 
 const LegacyAgreedPrice = s.object({
-    type: s.union([s.literal('legacy-agreed-price'), s.literal('legacy-agreed-price')])
+    type: s.literal('legacy-agreed-price')
 })
 
 module.exports = {
