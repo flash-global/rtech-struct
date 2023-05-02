cd8532e77ecdef28351242bce1de5833f9c6b87b
miranto <m.razafindrazaka@redspher.com[core]>
2023-05-02 16:08:08 +0200

    fix wrong property type

--- definition/request/ValidityTime.ts
+++ definition/request/ValidityTime.ts
@@ -1,7 +1,10 @@
 type ValidityTime = {
     valid_from?: string,
     valid_until?: string,
-    decision_time?: string,
+    decision_time?: {
+        decision_from: string,
+        close_after?: boolean
+    },
 };
 
 export default ValidityTime;
