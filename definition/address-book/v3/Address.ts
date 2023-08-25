4863531fb821e16d22b1d884c17cd504427e978b
Timur Symzhitov <t.symzhitov@redspher.com>
2023-08-25 19:23:49 +0900

    Optional score

--- definition/address-book/v3/Address.ts
+++ definition/address-book/v3/Address.ts
@@ -5,7 +5,7 @@ import Score from './Score';
 type Address = InputAddress & {
   id: string,
   contacts: Contact[],
-  score: Score,
+  score?: Score,
   created_at: string,
   updated_at: string
 };
