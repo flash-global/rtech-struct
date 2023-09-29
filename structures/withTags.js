02ef617df33ee208ab0d5eb38b1c76ec8795c458
GBo <g.boulangeot@redspher.com>
2023-09-29 18:31:35 +0200

    added JSDoc

--- structures/withTags.js
+++ structures/withTags.js
@@ -1,5 +1,15 @@
 const { type, optional, array, enums, number, refine } = require('superstruct')
 
+/**
+ * Creates a superstruct type for an object with an optional tags property.
+ * The tags property is an array of strings, and its values are validated against the authorizedTags array.
+ * If maxTags is defined, the length of the tags array is also validated to ensure it doesn't exceed maxTags.
+ *
+ * @param {Object} config - Configuration object for the struct.
+ * @param {string[]} [config.authorizedTags] - Array of authorized tag strings. Defaults to an empty array if not provided.
+ * @param {number | undefined} [config.maxTags] - Maximum number of tags allowed. If defined, the length of the tags array should not exceed this number. If undefined, then there's no limit of the quantity of tags.
+ * @returns {superstruct.Struct} A superstruct type representing the struct.
+ */
 exports.withTags = (config = {}) => {
   const authorizedTags = Array.isArray(config?.authorizedTags) ? config.authorizedTags : []
 
