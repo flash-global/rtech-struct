5ef6931bf29648af7c0ec3e84f84a2cfa6dbb421
Bharath <b.ganesan@redspher.com>
2025-03-25 17:05:17 +0100

    Request v3: Optional distance for single step

---
+++ structures/requested_transport_v3.js
@@ -0,0 +1,31 @@
+const s = require('superstruct')
+const { NoEmptyString } = require('./string')
+
+// existing validation
+const validateDistance = (distances) => {
+  if (Array.isArray(distances) && distances.length >= 1 && distances.length <= 10) {
+    return distances.every((distance) => {
+      return typeof distance === 'number' && distance >= 0
+    })
+  }
+
+  return false
+}
+
+const RequestedTransport = s.object({
+  way: s.size(s.array(NoEmptyString), 2, 50),
+  vehicles: s.defaulted(s.optional(s.size(s.array(NoEmptyString), 0, 15)), []),
+  distances: s.refine(s.any(), 'Distances', (distances, ctx) => {
+    const payload = ctx.branch[0]
+
+    if (payload.transports && payload.transports.length === 1 && payload.transports[0].way.length == 2) {
+      return distances === undefined || distances.length === 0 || validateDistance(distances)
+    }
+
+    return validateDistance(distances)
+  })
+})
+
+module.exports = {
+  RequestedTransport: RequestedTransport
+}
